b325a58a9c2ca8a5ec03b4d86c4553ed7386656f
Jisoo Yoo <youzysu@gmail.com>
2023-06-09 00:21:21 +0900

    feat: 메인 아티클에 마우스 호버 시 썸네일 이미지 확대 및 뉴스 타이틀 밑줄 효과

--- src/components/MediaViewer/ListViewer/PressArticle/MainArticle.ts
+++ src/components/MediaViewer/ListViewer/PressArticle/MainArticle.ts
@@ -8,16 +8,18 @@ interface MainArticleProps {
 
 export default class MainArticle {
   public readonly element = createElement('DIV', { class: styles.mainArticle });
-  private thumbnail = createElement('IMG', { class: styles.thumbnail });
+  private thumbnail = createElement('DIV', { class: styles.thumbnail });
+  private thumbnailImg = createElement('IMG', { class: styles.thumbnailImg });
   private title = createElement('A', { class: `body-md ${styles.mainTitle}` });
 
   constructor() {
+    this.thumbnail.append(this.thumbnailImg);
     this.element.append(this.thumbnail, this.title);
   }
 
   public render({ thumbnail, mainArticle }: MainArticleProps) {
-    this.thumbnail.setAttribute('src', thumbnail.img);
-    this.thumbnail.setAttribute('alt', thumbnail.title);
+    this.thumbnailImg.setAttribute('src', thumbnail.img);
+    this.thumbnailImg.setAttribute('alt', thumbnail.title);
     this.title.setAttribute('href', mainArticle.link);
     this.title.textContent = mainArticle.title;
   }
